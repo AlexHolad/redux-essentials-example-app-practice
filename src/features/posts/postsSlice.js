01d9397175a4b20da47406d589c809b90d670c85
alexholad <alexej.holad@gmail.com>
2023-05-06 14:51:24 +0200

    added asyncThunk for fetching posts from fakeapi extra reducers to handle async actions and changed component structure to display spinner and data after fetching

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,11 +1,18 @@
-import { createSlice, nanoid } from '@reduxjs/toolkit'
-import { sub } from 'date-fns'
+import { createSlice, nanoid, createAsyncThunk } from '@reduxjs/toolkit'
 
 
-const initialState = [
-  { id: '1', title: 'First Post!', content: 'Hello!', date: sub(new Date(), { minutes: 10 }).toISOString(), reactions: {thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0} },
-  { id: '2', title: 'Second Post', content: 'More text', date: sub(new Date(), { minutes: 5 }).toISOString(), reactions: {thumbsUp: 0, hooray: 0, heart: 3, rocket: 0, eyes: 0} },
-]
+import {client} from "../../api/client"
+
+const initialState = {
+  posts: [],
+  status: 'idle',
+  error: null
+}
+
+export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
+  const response = await client.get('/fakeApi/posts')
+  return response.data
+})
 
 const postsSlice = createSlice({
   name: 'posts',
@@ -13,7 +20,7 @@ const postsSlice = createSlice({
   reducers: {
     postAdded: {
       reducer(state, action) {
-        state.push(action.payload)
+        state.posts.push(action.payload)
       },
       prepare(title, content, userId) {
         return {
@@ -30,7 +37,7 @@ const postsSlice = createSlice({
     },
     postUpdated(state, action) {
       const { id, title, content } = action.payload
-      const existingPost = state.find((post) => post.id === id)
+      const existingPost = state.posts.find((post) => post.id === id)
 
       if (existingPost) {
         existingPost.title = title
@@ -39,14 +46,40 @@ const postsSlice = createSlice({
     },
     reactionAdded(state, action) {
       const { postId, reaction } = action.payload
-      const existingPost = state.find(post => post.id === postId)
+      const existingPost = state.posts.find(post => post.id === postId)
       if (existingPost) {
         existingPost.reactions[reaction]++
       }
     }
   },
+  extraReducers(builder) {
+    builder
+      .addCase(fetchPosts.pending, (state, action) => {
+        state.status = 'loading'
+      })
+      .addCase(fetchPosts.fulfilled, (state, action) => {
+        state.status = 'succeeded'
+        // Add any fetched posts to the array
+        state.posts = state.posts.concat(action.payload)
+      })
+      .addCase(fetchPosts.rejected, (state, action) => {
+        state.status = 'failed'
+        state.error = action.error.message
+      })
+  }
 })
 
 export const { postAdded, postUpdated, reactionAdded } = postsSlice.actions
 
 export default postsSlice.reducer
+
+// REUSABLE SELECTOR FUNCTIONS - used to avoid rewriting our components every time we made a change to the data format in our reducers. 
+// One way to avoid this is to define reusable selector functions in the slice files, 
+// and have the components use those selectors to extract the data they need
+//  instead of repeating the selector logic in each component. 
+//  That way, if we do change our state structure again, we only need to update the code in the slice file.   
+
+export const selectAllPosts = state => state.posts.posts
+
+export const selectPostById = (state, postId) => 
+state.posts.posts.find(post => post.id === postId)
