0052ed92de736f885cd3f1e84e6fb07e40b387bf
alexholad <alexej.holad@gmail.com>
2023-05-07 11:46:10 +0200

    added UserPage component and UsersList component and also 2 selectors to dislay list of users and user page

--- src/app/Navbar.js
+++ src/app/Navbar.js
@@ -9,8 +9,9 @@ export const Navbar = () => {
         <h1>Redux Essentials Example</h1>
 
         <div className="navContent">
-        <div className="navLinks">
+          <div className="navLinks">
             <Link to="/">Posts</Link>
+            <Link to="/users">Users</Link>
           </div>
         </div>
       </section>
--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,18 +1,29 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import { client } from '../../api/client'
+import { useSelector } from 'react-redux'
 
-const initialState = [
-    { id: '0', name: 'Tianna Jenkins' },
-    { id: '1', name: 'Kevin Grant' },
-    { id: '2', name: 'Madison Price' }
-  ]
+const initialState = []
 
-const usersSlice = createSlice({
-    name: 'users',
-    initialState,
-    reducers: {
-
-    }
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
+  const response = await client.get('/fakeApi/users')
+  return response.data
 })
 
+const usersSlice = createSlice({
+  name: 'users',
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      return action.payload
+    })
+  }
+})
 
 export default usersSlice.reducer
+
+// REUSABLE SELECTOR FUNCTIONS
+
+export const selectAllUsers = state => state.users
+
+export const selectUserById = (state, userId) => state.users.find(user => user.id === userId)
